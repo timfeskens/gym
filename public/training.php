0eee8e247a1fdfd0c8eb1b6b40ac013634b3303f
Tim Feskens <timf_1998@hotmail.com>
2018-03-15 15:46:45 +0100

    start on training.php

---
+++ public/training.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" href="css/main.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Index</title>
+</head>
+<body>
+	<div class="training">
+		<a href="exercise.php?id=1" class="training__block">
+			<div class="training__title">
+				chest
+			</div>
+		</a>
+		<a href="exercise.php?id=1" class="training__block">
+			<div class="training__title">
+				back
+			</div>
+		</a>
+		<a href="exercise.php?id=1" class="training__block">
+			<div class="training__title">
+				bicep
+			</div>
+		</a>
+		<a href="exercise.php?id=1" class="training__block">
+			<div class="training__title">
+				tricep
+			</div>
+		</a>
+		<a href="exercise.php?id=1" class="training__block">
+			<div class="training__title">
+				shoulder
+			</div>
+		</a>
+		<a href="exercise.php?id=1" class="training__block">
+			<div class="training__title">
+				legs
+			</div>
+		</a>
+		<a href="exercise.php?id=1" class="training__block">
+			<div class="training__title">
+				abs
+			</div>
+		</a>
+		<a href="exercise.php?id=1" class="training__block">
+			<div class="training__title">
+				other
+			</div>
+		</a>
+	</div>
+</body>
+</html>
