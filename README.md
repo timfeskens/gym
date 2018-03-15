90a7e75db4b15e5e73df18cd2e010f84504c9f74
Tim Feskens <timf_1998@hotmail.com>
2018-03-15 11:13:35 +0100

    first commit yay

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# gym
-gym maat
+# GYM MAAT
+
+## Running the project
+
+Before you run the project, be sure to do ``npm i`` inside the root folder.
+
+You can run this project without any plugins with this command:
+`` php -S localhost:8000 `` 
+
+> Make sure you run this command inside the _public_ folder.
---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./resources/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./resources/sass/**/*.scss', ['sass']);
+});
+
+// The default task (called when you run `gulp` from cli) 
+gulp.task('default', ['sass:watch']);
