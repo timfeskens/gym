875a21fafc800600716a00dcf6dacd864effdb82
Tim Feskens <timf_1998@hotmail.com>
2018-03-15 13:30:13 +0100

    make a login page yo

--- public/index.php
+++ public/index.php
@@ -2,7 +2,6 @@
 <html lang="en">
 <head>
 	<link rel="stylesheet" href="css/main.css">
-	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
---
+++ public/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" href="css/main.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Log in</title>
+</head>
+<body>
+	<div class="login">
+		<div class="login__block">
+			<div class="login__photo">
+				<i class="material-icons login__icon">perm_identity</i>
+			</div>
+		</div>
+		<form action="" method="post" class="login__block login__block--inputs">
+			<input type="email" class="login__input" placeholder="Email" required>
+			<input type="password" class="login__input" placeholder="Password"  required>
+			<button type="submit" class="login__input login__input--button">Log in</button>
+		</form>
+	</div>
+</body>
+</html>
