4768e39412aeb21ccea4c790baa08638ccec3a27
Tim Feskens <timf_1998@hotmail.com>
2018-03-15 12:20:51 +0100

    first styling yo

--- public/index.php
+++ public/index.php
@@ -1,13 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="css/main.css">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Index</title>
+	<link rel="stylesheet" href="css/main.css">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Index</title>
 </head>
 <body>
-    
+	<div class="home">
+		<a href="training.php" class="home__block home__block--training">
+			<div class="home__title">
+				<i class="material-icons home__icon">fitness_center</i>
+			</div>
+		</a>
+		<a href="stats.php" class="home__block home__block--stats">
+			<div class="home__title">
+				<i class="material-icons home__icon">poll</i>
+			</div>
+		</a>
+		<a href="profile.php" class="home__block home__block--profile">
+			<div class="home__title">
+				<i class="material-icons home__icon">perm_identity</i>
+			</div>
+		</a>
+	</div>
 </body>
 </html>
