964b0524c7df72c2dfff495be583f92823f3eb64
Tim Feskens <timf_1998@hotmail.com>
2018-03-22 14:40:48 +0100

    read out data on the excersises page

--- public/exercises.php
+++ public/exercises.php
@@ -22,25 +22,28 @@ $result = mysqli_query($mysqli, "SELECT * FROM exercises WHERE bodypart_id = '$b
 			<div class="exercises__title">
 				<?= $row['name']; ?>
 			</div>
-			<i class="material-icons exercises__icon">keyboard_arrow_down</i>
-			<div class="exercises__content">
+			<i class="material-icons exercises__icon exercises__icon--open">keyboard_arrow_down</i>
+		</div>
+		<div class="exercises__content">
 				<div class="exercises__set-column">
 					<?php 
 						$exerciseID = $row['id'];
 						$exerciseResult = mysqli_query($mysqli, "SELECT * FROM user_exercises WHERE exercise_id = '$exerciseID' AND user_id = '1'");
 
-						if ( count ($row) == 0) {
-							echo 'No records for this exercise';
-						} else {
 						while ($rows = mysqli_fetch_array($exerciseResult)) {
 							$setID = $rows['set_id'];
 							$setsResult = mysqli_query($mysqli, "SELECT * FROM sets WHERE id = '$setID'");
 
 							$row = mysqli_fetch_array($setsResult);
+							?>
+							<div class="exercises__set-row">
+								<div class="exercises__set-number">
+									<?= $row['set_number'] ?>
+								</div> 
+								<?= $row['reps'] . 'x' . $row['weight'] . '<br>';?>
+							</div>
+							<?php
 							
-							
-								echo 'Set ' . $row['set_number'] .': ' . $row['reps'] . 'x' . $row['weight'] . '<br>';
-							}
 						}
 					?>
 				</div>
@@ -48,8 +51,27 @@ $result = mysqli_query($mysqli, "SELECT * FROM exercises WHERE bodypart_id = '$b
 					<i class="material-icons exercises__icon exercises__icon--edit">mode_edit</i>
 				</div>
 			</div>
-		</div>
 		<?php } ?>
+		<a href="excersises_add.php?id=<?= $bodypart_id; ?>" class="exercises__add">
+			<i class="material-icons exercises__icon exercises__icon--add">add_box</i>
+		</a>
 	</div>
+	<script>
+		let dropdown = document.getElementsByClassName( 'exercises__block' );
+		let dropdownContent = document.getElementsByClassName( 'exercises__content' );
+		let icon = document.getElementsByClassName( 'exercises__icon--open' );
+		
+		for ( let i = 0; i < dropdown.length; i++) {
+			dropdown[i].addEventListener( "click", function() {
+				if ( dropdownContent[i].style.display == 'flex' ) {
+					dropdownContent[i].style.display = 'none';
+					icon[i].style.transform = "rotate(0deg)"; 
+				} else {
+					dropdownContent[i].style.display = 'flex';
+					icon[i].style.transform = "rotate(180deg)";
+				}
+			});
+		}
+	</script>
 </body>
 </html>
