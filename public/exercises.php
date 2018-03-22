86c73bea68c730da62f5f7b842f5739d0225d0e2
Tim Feskens <timf_1998@hotmail.com>
2018-03-22 11:36:53 +0100

    merge develop

--- public/exercises.php
+++ public/exercises.php
@@ -3,13 +3,9 @@ require 'require_session.php';
 
 $bodypart_id = $_GET['id'];
 
-$query = "SELECT * FROM exercises WHERE bodypart_id = '$bodypart_id'";
-$result = mysqli_query($mysqli, $query);
-
-while ($row = mysqli_fetch_array($result)) {
-	echo $row['name'];
-}
+$result = mysqli_query($mysqli, "SELECT * FROM exercises WHERE bodypart_id = '$bodypart_id'");
 ?>
+<?php require 'require_session.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,7 +17,39 @@ while ($row = mysqli_fetch_array($result)) {
 </head>
 <body>
 	<div class="exercises">
+		<?php while ($row = mysqli_fetch_array($result)) { ?>
+		<div class="exercises__block">
+			<div class="exercises__title">
+				<?= $row['name']; ?>
+			</div>
+			<i class="material-icons exercises__icon">keyboard_arrow_down</i>
+			<div class="exercises__content">
+				<div class="exercises__set-column">
+					<?php 
+						$exerciseID = $row['id'];
+						$exerciseResult = mysqli_query($mysqli, "SELECT * FROM user_exercises WHERE exercise_id = '$exerciseID' AND user_id = '1'");
 
+						if ( count ($row) == 0) {
+							echo 'No records for this exercise';
+						} else {
+						while ($rows = mysqli_fetch_array($exerciseResult)) {
+							$setID = $rows['set_id'];
+							$setsResult = mysqli_query($mysqli, "SELECT * FROM sets WHERE id = '$setID'");
+
+							$row = mysqli_fetch_array($setsResult);
+							
+							
+								echo 'Set ' . $row['set_number'] .': ' . $row['reps'] . 'x' . $row['weight'] . '<br>';
+							}
+						}
+					?>
+				</div>
+				<div class="exercises__set-column">
+					<i class="material-icons exercises__icon exercises__icon--edit">mode_edit</i>
+				</div>
+			</div>
+		</div>
+		<?php } ?>
 	</div>
 </body>
 </html>
